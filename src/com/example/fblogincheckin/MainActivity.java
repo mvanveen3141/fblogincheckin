d74185e0d4c57059ca0e346d9408fd25c22ca051
Mark van Veen <mvanveen3141@gmail.com>
2012-12-03 02:38:05 -0800

    login successful

--- src/com/example/fblogincheckin/MainActivity.java
+++ src/com/example/fblogincheckin/MainActivity.java
@@ -1,20 +1,57 @@
 package com.example.fblogincheckin;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
 
+//instead of android.app.Activity, facebook uses:
+import com.facebook.FacebookActivity;
+//to allow sessions (login) with facebook:
+import com.facebook.SessionState;
+import com.facebook.GraphUser;
+import com.facebook.Request;
+import com.facebook.Response;
+
+
+public class MainActivity extends FacebookActivity { 
+									//notice parent class, FacebookActivity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        		//this.openSession >>> facebook login, begins session w/fb
+        this.openSession(); 
+        		//Once the user is logged in, 
+        		//the onSessionStateChange method of the activity will be called.
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    @Override
+    protected void onSessionStateChange(SessionState state, Exception exception) {
+      // after login or no login
+	  if (state.isOpened()) {
+		    // make request to the /me API
+		    Request request = Request.newMeRequest(
+		      this.getSession(),
+		      new Request.GraphUserCallback() {
+		        // callback after Graph API response with user object
+		        @Override
+		        public void onCompleted(GraphUser user, Response response) {
+		          if (user != null) {
+		            TextView welcome = (TextView) findViewById(R.id.welcome);
+		            welcome.setText("Hello " + user.getName() + "!");
+		          }
+		        }
+		      }
+		    );
+		    Request.executeBatchAsync(request);
+		  }
     }
 }
